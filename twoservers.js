a7891bb7aaf0fb8517cf8b305d32c885931b3842
Jacob Yolles <jacobyolles@Jacobs-MacBook-Pro.local>
2018-09-05 10:41:23 -0600

    file added

---
+++ twoservers.js
@@ -0,0 +1,37 @@
+var http = require("http");
+
+var goodPORT = 7000;
+var badPORT = 7500;
+
+function handleRequest(request, response) {
+
+    // send this string to the client.
+    response.end("You always look really pretty:" + request.url);
+    
+}
+
+// gives the server functionality.
+var server = http.createServer(handleRequest);
+
+//start server so it begins to listen to client requests
+server.listen(goodPORT, function() {
+    console.log("Server listening on : http://localhost:" + goodPORT);
+    
+})
+
+
+function handleRequest(request, response) {
+
+    // send this string to the client.
+    response.end("You should shower more:" + request.url);
+    
+}
+
+// gives the server functionality.
+var server = http.createServer(handleRequest);
+
+//start server so it begins to listen to client requests
+server.listen(badPORT, function() {
+    console.log("Server listening on : http://localhost:" + badPORT);
+    
+})
